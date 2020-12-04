f3a0bbbf4e8b39f590b1242e17be577e5db500b6
Jerry Chien <64075878+juck30808@users.noreply.github.com>
2020-12-04 18:13:35 +0800

    1204

--- 2.R-tech/RtechSTD_Team.R
+++ 2.R-tech/RtechSTD_Team.R
@@ -3,7 +3,6 @@
 
 
 #####===== (1) (KDD1) 讀取數據(-->X) =====#####
-
 library(readxl)
 X <- as.data.frame(read_excel('/Users/juck30808/Documents/Github/USC_R_Git/2.R-tech/data/googleplaystore2.xlsX'))
 dim(X);   
@@ -11,10 +10,8 @@ head(X,2)
 
 
 #####===== (2) (KDD2-3) 數據轉換(X-->XX) =====#####
-
 range(X$Rating) 
 table( cut(X$Rating, breaks=c(0,1,2,3,4,5)) )
-
 
 range(X$Year)   #2010 - 2018
 table(X$Year)
@@ -23,8 +20,6 @@ rownames(XX) = 1:dim(XX)[1]
 
 
 #####===== (3) (KDD4) 數據模型(XX-->XX.group) =====#####
-
-
 X.hc = hclust( dist( X[,c("Rating","Reviews","Installs")] ),method="ward.D")
 X.group = cutree(X.hc, k=20)
 
@@ -43,11 +38,11 @@ round(apply(X[which(X.group==1),c("Rating","Reviews","Installs")], 2, sd),2)
 # 4.11   145.01  4646.57 
 # 0.73   360.82  4272.84 
 
+X.group
 
 
 #####===== (4) (KDD5) 數據解讀(XX.group) =====#####
-
-kk = 1
+kk = 1  #X.group(kk)
 indKK = which(X.group==kk);   indKK
 c(kk,length(indKK), apply(X[indKK,c("Rating","Reviews","Installs")], 2, mean))
 
@@ -73,12 +68,9 @@ round(Gmean[1,],2)
 ##-- + 145.01 * Reviews
 ##-- + 4646.57 * Installs
 
-colnames(Gmean)[3:5]   #表頭
-round(Gmean[1,3:5],2)  #加權 
-
-AA = paste0(round(Gmean[1,3:5],1), "*", colnames(Gmean)[3:6]);  AA
-BB = paste( AA, collapse="+");   BB
-# [1] "10.3*NA_Sales+4.5*EU_Sales+0.1*JP_Sales+1.3*Other_Sales"
+AA = paste0(round(Gmean[1,3:5],2), " * ", colnames(Gmean)[3:5]);  AA
+BB = paste( AA, collapse=" + ");   BB
+# "4.1 * Rating + 145 * Reviews + 4646.6 * Installs"
 
 
 #####===== (5) (KDD5) 數據解讀(XX.group) =====#####
@@ -87,7 +79,7 @@ BB = paste( AA, collapse="+");   BB
 Gfeature = NULL
 for (k in 1:dim(Gmean)[1]) {
   print(k)
-  AA = paste0(round(Gmean[k,3:5],2), "*", colnames(Gmean)[3:5]);  AA
+  AA = paste0(round(Gmean[k,3:5],2), " * ", colnames(Gmean)[3:5]);  AA
   BB = paste( AA, collapse="+")
   print(BB)
   Gfeature[k] = BB
@@ -100,7 +92,7 @@ colnames(Gm)[1:2] = c("ind","count");   head(Gm)
 Gm$feature = Gfeature
 head(Gm)
 
-# ind count   Rating     Reviews     Installs                                       feature
+#   ind count   Rating     Reviews     Installs                                       feature
 # 1   1  2703 4.114058    145.0067     4646.566   4.11*Rating+145.01*Reviews+4646.57*Installs
 # 2   2   487 4.166530   9483.6181   500000.000    4.17*Rating+9483.62*Reviews+5e+05*Installs
 # 3   3   525 4.248952 106100.6076  5000000.000  4.25*Rating+106100.61*Reviews+5e+06*Installs
@@ -109,5 +101,3 @@ head(Gm)
 # 6   6  1294 4.219706  33331.1955  1000000.000    4.22*Rating+33331.2*Reviews+1e+06*Installs
 
 write.csv(Gm,"Team1.csv")
-
-
